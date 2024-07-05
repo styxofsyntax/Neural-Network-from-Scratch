623ceb688c616a1eaf628ca40f55a5214eee51fc
styxofsyntax <107263899+styxofsyntax@users.noreply.github.com>
2024-07-05 17:36:05 +0500

    Implement and test back propagation
    neural network
    - Add back propagation method
    - Verify training of model on MNIST dataset
    - Use HE Weight Initialization
    
    activations
    - Add derivative of ReLU and softmax
    - Add numerically stable version of softmax

--- activations.py
+++ activations.py
@@ -6,11 +6,26 @@ def relu(z):
 
 
 def softmax(z):
-    expZ = np.exp(z)
+    z_max = np.max(z, axis=1, keepdims=True)
+    expZ = np.exp(z - z_max)
     return expZ / np.sum(expZ, axis=1, keepdims=True)
+
+
+def deriv_relu(z):
+    return z > 0
+
+
+def deriv_softmax(z):
+    s = softmax(z)
+    return s * (1 - s)
 
 
 activations = {
     'relu': relu,
     'softmax': softmax
+}
+
+deriv_activations = {
+    'relu': deriv_relu,
+    'softmax': deriv_softmax
 }
--- neural_network.py
+++ neural_network.py
@@ -1,5 +1,5 @@
 import numpy as np
-from activations import activations
+from activations import activations, deriv_activations
 
 
 class Layer:
@@ -8,8 +8,10 @@ class Layer:
         self.num_neurons = num_neurons
         self.activation = activation
 
-        self.weight = np.random.randn(num_inputs, num_neurons)
-        self.bias = np.random.randn(num_neurons)
+        std = np.sqrt(2. / num_inputs)
+        self.weight = np.random.randn(
+            num_inputs, num_neurons) * std
+        self.bias = np.zeros(num_neurons)
 
         self.z = 0
         self.output = 0
@@ -26,18 +28,18 @@ class NeuralNetwork:
         layer_inputs = num_inputs
 
         for i in range(len(layers)):
-            l = Layer(layer_inputs, layers[i][0], layers[i][1])
-            # next layer inputs are equal to previous layers number of neurons
+            layer = Layer(layer_inputs, layers[i][0], layers[i][1])
+            # Next layer inputs are equal to previous layers number of neurons
             layer_inputs = layers[i][0]
 
-            self.layers.append(l)
+            self.layers.append(layer)
 
     def summary(self):
         for layer in self.layers:
             print(f'inputs: {layer.num_inputs} - neurons: {layer.num_neurons}')
 
     def forward_prop(self, inputs):
-        current_input = inputs
+        current_input = np.copy(inputs)
 
         for layer in self.layers:
             layer.z = np.dot(current_input, layer.weight) + layer.bias
@@ -47,3 +49,43 @@ class NeuralNetwork:
             current_input = layer.output
 
         return self.layers[-1].output
+
+    def backward_prop(self, inputs, outputs, alpha):
+        diff = self.layers[-1].output - outputs
+        dE_a = (2.0 * diff) / len(outputs)
+
+        for i in range(len(self.layers))[::-1]:
+
+            da_z = deriv_activations[self.layers[i].activation](
+                self.layers[i].z)
+
+            dz_a_prev = self.layers[i].weight
+
+            dz_w = self.layers[i-1].output if i > 0 else np.copy(inputs)
+
+            dE_z = dE_a * da_z
+
+            dE_w = np.dot(dz_w.T, dE_z)
+
+            dE_b = np.sum(dE_z, axis=0)
+            dE_b = dE_b.squeeze()
+
+            # Update weights and biases
+            self.layers[i].weight -= alpha * dE_w
+            self.layers[i].bias -= alpha * dE_b
+
+            # Propagate the error to the previous layer
+            dE_a = np.dot(dE_z, dz_a_prev.T)
+
+    def fit(self, inputs, outputs, epoch, alpha):
+        for i in range(epoch):
+            self.forward_prop(inputs)
+
+            if i % (epoch // 10) == 0:
+                loss = self.calculate_loss(self.layers[-1].output, outputs)
+                print(f'loss: {loss}')
+
+            self.backward_prop(inputs, outputs, alpha)
+
+    def calculate_loss(self, outputs, targets):
+        return np.mean(np.square(outputs - targets))
